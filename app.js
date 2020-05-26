13a04f6f6c32f8144390ee4bd62019e3a64e6cb3
Robert Lowe <robertmlowe@rmlowe.com>
2020-05-26 23:51:38 +0800

    Asyn/Await Demo

--- app.js
+++ app.js
@@ -1,14 +1,24 @@
 const getItems = () => {
-    return new Promise((resolve, reject) => {
-        setTimeout(() => {
-            resolve(['Mobile', 'Mac', 'Pen', 'Book']);
-        }, 1000);
-    });
+  return new Promise((resolve, reject) => {
+    setTimeout(() => {
+      resolve(['Mobile', 'Mac', 'Pen', 'Book']);
+    }, 1000);
+  });
 };
 
-getItems()
-    .then(results => {
-        console.log(results);
-    })
-    .catch(err => console.error(err));
+const processRequest = async () => {
+  const items1 = await getItems();
+  const items2 = await getItems();
+  const items3 = await getItems();
+  const items4 = await getItems();
+  const items5 = await getItems();
+  const items6 = await getItems();
+  const items7 = await getItems();
+  return [items1, items2, items3, items4, items5, items6, items7];
+};
+
+processRequest()
+  .then(results => console.log(results))
+  .catch(err => console.error(err));
+
 
